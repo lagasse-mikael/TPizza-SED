79bb8825d574522cd12a72cf9848c0639fb5688a
Amine K <1643859@cstj.qc.ca>
2021-11-26 16:08:42 -0500

    Premiere route B faite

--- src/repositories/pizzeria.repo.js
+++ src/repositories/pizzeria.repo.js
@@ -3,7 +3,35 @@ import Pizzeria from '../models/pizzeria.model.js';
 // import dayjs from 'dayjs';
 
 class PizzeriaRepository {
-    retrieveByID(id){
+
+    retrieveAllBySpeciality(retrieveOptions) {
+
+        let speci = `${retrieveOptions.speciality}`
+        const retrieveQuery = Pizzeria.find()
+        .skip(retrieveOptions.skip).limit(retrieveOptions.limit)
+        .where('chef.speciality').equals(retrieveOptions.speciality)
+        .sort('chef.name')
+        const countQuery = Pizzeria.find({'chef.speciality':retrieveOptions.speciality}).count();
+        
+        return Promise.all([retrieveQuery, countQuery]);
+
+
+    }
+
+    retrieveAll(retrieveOptions) {
+
+        const retrieveQuery = Pizzeria.find()
+            .skip(retrieveOptions.skip).limit(retrieveOptions.limit)
+            .sort('chef.name')
+        const countQuery = Pizzeria.countDocuments();
+
+        return Promise.all([retrieveQuery, countQuery]);
+
+
+    }
+
+
+    retrieveByID(id) {
         const retrieveResponse = Pizzeria.findById(id)
 
         return retrieveResponse
